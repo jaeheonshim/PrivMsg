3d2aad8aff968d313dad3681e440cd1cbb62da46
Jaeheon Shim <git@jaeheonshim.com>
2020-05-17 14:37:46 -0400

    Initial commit

---
+++ src/main/java/com/jaeheonshim/privmsg/MessagePlugin.java
@@ -0,0 +1,10 @@
+package com.jaeheonshim.privmsg;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MessagePlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+}
